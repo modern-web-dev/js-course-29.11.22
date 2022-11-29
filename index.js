b0f5bc57013e621e82522248f08b70c7fcdec7ca
mmatczak <marek.matczak79@gmail.com>
2022-11-29 09:54:47 +0100

    objects

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('Hello world!');
+let marek = {
+    name: 'Marek',
+    age: 0,
+    dateOfBirth: new Date('1979-01-25'),
+    worksForCapgemini: true,
+    address: {
+        city: 'Wroclaw',
+        zipCode: '53-601'
+    }
+};
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const czarek = new Person('Czarek', 33);
+// marek.address = undefined;
+// marek = undefined;
+// const zipCode = marek.address.zipCode;
+// falsy values in JS: null, undefined, 0, '', NaN, false
+// const zipCode = (marek && marek.address && marek.address.zipCode) || 'NO ZIP CODE';
+// delete marek.age;
+const age = marek?.age ?? 'NO AGE';
+console.log(age);
+
+if (marek.name != null) {
+
+}
+
+
+// delete marek.name;
+// console.log(marek['name']);
+// console.log(czarek);
+
